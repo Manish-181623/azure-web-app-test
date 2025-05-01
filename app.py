a717cf23d956c9e7a16614752425036e35a16d1b
MSI <manish@inspiringlab.com.np>
2025-05-01 15:33:33 +0545

    Updated app.py file

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def create_app():
     @app.route('/')
     def hello():
         return "Hello from Azure Web App! This is a Python Flask demo.Is this working?" \
-        "Yes, it is working!"
+        "Yes, it is working fine!"
     
     @app.route('/about')
     def about():
